3de3b5a85ef7e7db99da01cbbaa0d18f19fb1e21
Haruhiro Matsumoto <haruhi.ro@icloud.com>
2025-10-07 11:54:10 +0900

    Initial Commit

---
+++ Percify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Percify
+//
+//  Created by 松本知大 on 2025/10/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Percify/PercifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PercifyApp.swift
+//  Percify
+//
+//  Created by 松本知大 on 2025/10/07.
+//
+
+import SwiftUI
+
+@main
+struct PercifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
